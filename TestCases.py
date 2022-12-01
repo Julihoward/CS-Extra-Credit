501c9a29e795865a9c67ad40c23e7235fd4684c9
Julihoward <107530137+Julihoward@users.noreply.github.com>
2022-12-01 17:16:24 -0500

    Create TestCases.py

---
+++ TestCases.py
@@ -0,0 +1,11 @@
+from Outfithelper import recommend_outfit
+
+assert recommend_outfit(9, 10) == None
+assert recommend_outfit(37, 15) == None
+assert recommend_outfit(73, 29) == None
+assert recommend_outfit(12, 6) == None
+assert recommend_outfit(45, 17) == None
+assert recommend_outfit(58, 24) == None
+assert recommend_outfit(28, -2) == None
+assert recommend_outfit(48, 17) == None
+assert recommend_outfit(98, 27) == None
