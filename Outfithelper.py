4175247e184041c30e346b7331e10c34e7bdb2e1
Julihoward <107530137+Julihoward@users.noreply.github.com>
2022-12-01 10:19:13 -0500

    Update Outfithelper.py

--- Outfithelper.py
+++ Outfithelper.py
@@ -6,29 +6,30 @@ print("This is an app to help with clothe recommendations based on the weather."
 rain = int(input("What is the chance of rain in your location today?"))
 
 temperature = input("What is the celsius temperature of your location today")
+def recommend_outfit(rain, temperature):
+    if rain > 30 and rain < 50:
+        print("You should have umbrella handy")
+        umbrella = Image.open("Umbrella.jpg")
+        umbrella.show()
+    elif rain > 50:
+        print("Strong chance it rains today, you should definitely keep a raincoat!")
+        raincoat =Image.open("Raincoat.jpg")
+        raincoat.show()
+    elif rain <30:
+        print("looks like it will be dry today!")
 
-if rain > 30 and rain < 50:
-    print("You should have umbrella handy")
-    umbrella = Image.open("Umbrella.jpg")
-    umbrella.show()
-elif rain > 50:
-    print("Strong chance it rains today, you should definitely keep a raincoat!")
-    raincoat =Image.open("Raincoat.jpg")
-    raincoat.show()
-elif rain <30:
-    print("looks like it will be dry today!")
-
-if temperature < 14:
-    print("You're going to have a cold day aheand, make sure to layer up!")
-    coldday = Image.open("Colddays.jpg")
-    coldday.show()
-elif temperature > 14 and temperature <19:
-    print("It's a cool day today, a cardigan is not a bad idea")
-    coolday = Image.open("Early fall.jpg")
-    coolday.show()
-else:
-    print("You'll have a warm day today. Light, airy clothes are the way to go")
-    airy = Image.open("For warmer weather.jpg")
-    airy.show()
+    if temperature < 14:
+        print("You're going to have a cold day aheand, make sure to layer up!")
+        coldday = Image.open("Colddays.jpg")
+        coldday.show()
+    elif temperature > 14 and temperature <19:
+        print("It's a cool day today, a cardigan is not a bad idea")
+        coolday = Image.open("Early fall.jpg")
+        coolday.show()
+    else:
+        print("You'll have a warm day today. Light, airy clothes are the way to go")
+        airy = Image.open("For warmer weather.jpg")
+        airy.show()
+recommend_outfit(rain, temperature)
     
     
